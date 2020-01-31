e06cc226d70e6700701b07bc7defc3ac78320d65
Donnchadh O'Donoghue <donnodon11@gmail.com>
2020-01-31 11:30:58 +0000

    Implement 'last' as a recursive function

--- src/main/scala/com/gv/scala/learning/list/Lists.scala
+++ src/main/scala/com/gv/scala/learning/list/Lists.scala
@@ -1,6 +1,13 @@
 package com.gv.scala.learning.list
 
 object Lists {
-  def last[A](list: List[A]): A = ???
-    //list(list.length - 1)
+  def last[A](list: List[A]): A = list match {
+    //case List.empty[A]
+      //head is first element of list tail is a sublist of list after head is taken out
+      //_ is everything
+    case Nil => throw new NoSuchElementException //no elements: list empty: list()
+    case head:: Nil => head //if there is one element i.e last("b")
+    case _::  tail => last(tail) //more than one element
+
+  }
 }
