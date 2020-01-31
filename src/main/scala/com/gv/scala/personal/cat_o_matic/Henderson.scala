da980eedd9b53dc6cc3775f5aa231e613f8db491
Donnchadh O'Donoghue <donnodon11@gmail.com>
2020-01-31 14:53:02 +0000

    Move Henderson, Oswald and Quentin to personal

---
+++ src/main/scala/com/gv/scala/personal/cat_o_matic/Henderson.scala
@@ -0,0 +1,6 @@
+package com.gv.scala.personal.cat_o_matic
+
+object Henderson {
+  val colour = "Ginger"
+  var food = "Chips"
+}
---
+++ src/main/scala/com/gv/scala/personal/cat_o_matic/Oswald.scala
@@ -0,0 +1,6 @@
+package com.gv.scala.personal.cat_o_matic
+
+object Oswald {
+  val colour = "Black"
+  var food = "Milk"
+}
