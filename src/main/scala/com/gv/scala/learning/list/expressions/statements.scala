553d2c0983fd5ea9055b1a0edf74be6c3a283c4c
Donnchadh O'Donoghue <donnodon11@gmail.com>
2020-01-31 14:25:56 +0000

    Add class with statement examples e.g if and else

---
+++ src/main/scala/com/gv/scala/learning/list/expressions/statements.scala
@@ -0,0 +1,12 @@
+package com.gv.scala.learning.list.classic.Rivalry
+
+class statements {
+  if(1 > 2) "alien" else "predator"
+  // res6: String = predator
+
+  if(1 > 2) "alien" else 2001
+  // res8: Any = 2001
+
+  if(false) "hello"
+  // res10: Any = ()
+}
