187073573cd6f931bbfda0481dd0a879d44a5ad7
Donnchadh O'Donoghue <donnodon11@gmail.com>
2020-01-31 10:49:13 +0000

    Test the exception thrown when called on empty list

--- src/test/scala/com/gv/scala/learning/list/ListTest.scala
+++ src/test/scala/com/gv/scala/learning/list/ListTest.scala
@@ -4,11 +4,16 @@ import org.scalatest.wordspec.AnyWordSpec
 
 class ListTest extends AnyWordSpec {
   "A list" when {
-    "empty" should {
+    "not empty" should {
       "yield the last element as requested " in{
         assertResult(5)(Lists.last(List(1,2,3,4,5)))
         assertResult("b")(Lists.last(List("a","b")))
       }
+    }
+    "empty" should {
+        "throws a NoSuchElement exception when last element is requested" in {
+          assertThrows[NoSuchElementException](Lists.last(List()))
+        }
     }
   }
 }
