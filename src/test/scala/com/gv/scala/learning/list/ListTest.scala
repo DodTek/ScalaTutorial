fdadf60d5e6d3633df60b5df3cf90e7fba8f4b6b
Donnchadh O'Donoghue <donnodon11@gmail.com>
2020-01-31 11:28:59 +0000

    Use construct notation in the test's list

--- src/test/scala/com/gv/scala/learning/list/ListTest.scala
+++ src/test/scala/com/gv/scala/learning/list/ListTest.scala
@@ -6,8 +6,9 @@ class ListTest extends AnyWordSpec {
   "A list" when {
     "not empty" should {
       "yield the last element as requested " in{
+        //List(1,2,3,4,5) = List(1 :: 2 :: 3 :: 4 :: 5 :: Nil
         assertResult(5)(Lists.last(List(1,2,3,4,5)))
-        assertResult("b")(Lists.last(List("a","b")))
+        assertResult("b")(Lists.last(List("a" :: "b" :: Nil)))
       }
     }
     "empty" should {
