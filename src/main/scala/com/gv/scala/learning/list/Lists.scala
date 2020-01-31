f8bc6a08e7385f4f3a643f57a970b961875979e6
Donnchadh O'Donoghue <donnodon11@gmail.com>
2020-01-31 12:01:16 +0000

    Add tail recursive function 'lenght'

--- src/main/scala/com/gv/scala/learning/list/Lists.scala
+++ src/main/scala/com/gv/scala/learning/list/Lists.scala
@@ -12,6 +12,13 @@ object Lists {
     case Nil => throw new NoSuchElementException //no elements: list empty: list()
     case head:: Nil => head //if there is one element i.e last("b")
     case _::  tail => last(tail) //more than one element
+  }
+
+  def lenght[A](list: List[A]): Int = list match {
+
+    case Nil => throw new NoSuchElementException //no elements: list empty: list()
+    case head :: Nil => +1 //if there is one element i.e last("b")
+    case _ :: tail => lenght(tail) //more than one element
 
   }
 }
