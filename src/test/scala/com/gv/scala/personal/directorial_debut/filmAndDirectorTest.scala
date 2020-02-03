1096391fb4d5f8d9b8dc91a6abd46f7cc972bffc
Donnchadh O'Donoghue <donnodon11@gmail.com>
2020-02-03 10:25:45 +0000

    Add test cases for director lastName and yearOfBirth

--- src/test/scala/com/gv/scala/personal/directorial_debut/filmAndDirectorTest.scala
+++ src/test/scala/com/gv/scala/personal/directorial_debut/filmAndDirectorTest.scala
@@ -13,18 +13,24 @@ class filmAndDirectorTest extends AnyWordSpec {
       }
     }
 
-  val memento           = new Film("Memento", 2000, 8.5, nolan)
-  val darkKnight        = new Film("Dark Knight", 2008, 9.0, nolan)
-  val inception         = new Film("Inception", 2010, 8.8, nolan)
+    "second name function is called" should {
+      "return last name" in {
+        assertResult("Eastwood")(eastwood.lastName)
+        assertResult("McTiernan")(mcTiernan.lastName)
+      }
+    }
+
+    "last name function is called" should {
+      "return date of birth" in {
+        assertResult("Eastwood")(eastwood.lastName)
+        assertResult(1951)(mcTiernan.yearOfBirth)
+      }
+    }
+  }
 
   val highPlainsDrifter = new Film("High Plains Drifter", 1973, 7.7, eastwood)
   val outlawJoseyWales  = new Film("The Outlaw Josey Wales", 1976, 7.9, eastwood)
-  val unforgiven        = new Film("Unforgiven", 1992, 8.3, eastwood)
-  val granTorino        = new Film("Gran Torino", 2008, 8.2, eastwood)
-  val invictus          = new Film("Invictus", 2009, 7.4, eastwood)
 
-  val predator          = new Film("Predator", 1987, 7.9, mcTiernan)
-  val dieHard           = new Film("Die Hard", 1988, 8.3, mcTiernan)
   val huntForRedOctober = new Film("The Hunt for Red October", 1990, 7.6, mcTiernan)
   val thomasCrownAffair = new Film("The Thomas Crown Affair", 1999, 6.8, mcTiernan)
 }
