87b5a1773740179493df4868f96f59c0d85523cf
Donnchadh O'Donoghue <donnodon11@gmail.com>
2020-02-04 09:59:32 +0000

    Add comments to functions

--- src/main/scala/com/gv/scala/learning/list/Lists.scala
+++ src/main/scala/com/gv/scala/learning/list/Lists.scala
@@ -24,10 +24,13 @@ object Lists {
   def isPalindrome(list:List[Int]):Boolean =
   {
     list match {
-      case Nil => true
-      //case {list.head != list.last} => false
-      case head:: Nil => isPalindrome(list.tail)
-      case list => (list.head == list.last && isPalindrome(list.tail.init))
+      case Nil => true//if list is empty list return true
+      case head:: Nil => isPalindrome(list.tail) //if list has one element return true
+      case list => (list.head == list.last && isPalindrome(list.tail.init))//if list contains multiple elements check if
+        //list head equals last element then recursively check the other elements
+        //note: head "removes" first element (1,2,3,2,1) becomes list(2,3,2,1)
+        //note: list.tail.init removes the tail i.e list(1,2,3,2,1) becomes list(1,2,3,2) after tail.init used
+
     }
   }
 
@@ -51,14 +54,15 @@ object Lists {
    */
 
   def flatten2(list: List[Any]): List[Any] = list match {
-    case List() => Nil
-    case head :: tail => {
-      head match {
-        case head: List[Any] => flatten2(head)
-        case _ => List(head)
+    case List() => Nil //if list empty return Nil
+
+    case head :: tail => {//if list has a head and tail
+      head match {//match head
+        case head: List[Any] => flatten2(head)//if head has a "tail" of any list flatten head
+        case _ => List(head) //if everything make list containing head
       }
-    } ::: flatten2(tail)
-    case _ => List(list)
+    } ::: flatten2(tail)//flatten tail and append to list head which should now be flattened
+    case _ => List(list)//if everything return list which should be flattened list of head and tail
   }
 
 }
